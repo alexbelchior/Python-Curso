ca578469797bf6e0402789756d530c3b1bf2b87f
alexbelchior <alexbelchior@gmail.com>
2025-02-13 19:12:45 -0300

    arquivos excel

--- Trabalhando com Arquivos/lendo_e_escrevendo_planilhas.py
+++ Trabalhando com Arquivos/lendo_e_escrevendo_planilhas.py
@@ -1,3 +1,37 @@
 from pathlib import Path
 import pandas as pd
 
+#Lendo tabelas com dataframe
+pasta_atual = Path(__file__).parent
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx')
+print(tabela_clientes.head(10)) # Exibe as primeiras 10 linhas da tabela
+
+#Lendo uma aba específica do arquivo excel
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='SP')
+print(tabela_clientes.head(10))
+
+#Modificando header para não exibir a primeira linha
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', header=1)
+print(tabela_clientes.head(10))
+
+#Adicionando coluna de index
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', index_col=0)
+print(tabela_clientes.head(10))
+
+#Lendo colunas específicas do arquivo excel
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', usecols="A:B")
+print(tabela_clientes.head(10))
+
+#Lendo colunas específicas do arquivo excel [2]
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', usecols=['Nome', 'Email'])
+print(tabela_clientes.head(10))
+
+#Representando valores decimais
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', usecols=['Nome', 'Receita'], decimal='.') # informa que o separador decimal é o ponto
+print(tabela_clientes.head(10))
+
+#tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ', usecols=['Nome', 'Receita'], thousands='.')
+
+#Escrevendo na planilha
+tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ')
+tabela_clientes.to_excel(pasta_atual / 'clientes_atualizado.xlsx', index=False)
