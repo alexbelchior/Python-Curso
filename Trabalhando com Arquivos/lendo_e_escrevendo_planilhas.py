a2970530c6d22b79f4c18d5b4b7ea7ebc18ffa0f
alexbelchior <alexbelchior@gmail.com>
2025-08-11 23:44:38 -0300

    atualizado

--- Trabalhando com Arquivos/lendo_e_escrevendo_planilhas.py
+++ Trabalhando com Arquivos/lendo_e_escrevendo_planilhas.py
@@ -36,14 +36,6 @@ print(tabela_clientes.head(10))
 tabela_clientes = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ')
 tabela_clientes.to_excel(pasta_atual / 'clientes_atualizado.xlsx', index=False)
 
-<<<<<<< HEAD
-#Escrevendo em várias abas
-tabela_clientes_sp = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='SP')
-tabela_clientes_rj = pd.read_excel(pasta_atual / 'clientes.xlsx', sheet_name='RJ')
-
-with pd.ExcelWriter(pasta_atual / 'clientes_atualizado_varias_abas.xlsx') as nova_planilha:
-    tabela_clientes_sp.to_excel(nova_planilha, sheet_name='SP', index=False)
-    tabela_clientes_rj.to_excel(nova_planilha, sheet_name='RJ', index=False)
 =======
 #Escrevendo diversas abas
 with pd.ExcelWriter(pasta_atual / 'clientes_atualizado.xlsx') as writer:
